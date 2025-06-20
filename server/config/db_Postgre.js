25fe14cdf7b956349e70cac9ee878b21b44800cc
Sam njoro <samnjorokibandi@gmail.com>
2025-06-20 23:38:57 +0300

    postgre db configuration and setup

---
+++ server/config/db_Postgre.js
@@ -0,0 +1,14 @@
+const { Pool } =  require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.SUPABASE_USER,
+    host: process.env.SUPABASE_HOST,
+    database: process.env.SUPABASE_DB,
+    password: process.env.POSTGRE_PASS,
+    ssl: {
+        rejectUnauthorized: false // Required for Supabase connection
+      }
+    });
+    
+module.exports = pool;
