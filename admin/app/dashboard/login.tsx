140be0c3a12e07cf7205ebe74f30c3cdeeb8905f
Sam njoro <samnjorokibandi@gmail.com>
2025-07-11 22:38:57 +0300

    login structure

--- admin/app/dashboard/login.tsx
+++ admin/app/dashboard/login.tsx
@@ -5,22 +5,68 @@ import axios from 'axios';
 
 export default function LoginAdmin(){
 
-    const [loading, setLoading] = useState(false);
-    const [ message, setMessage] = useState('')
+    const [ loading, setLoading ] = useState(false);
+    const [ message, setMessage ] = useState('');
+    const [ formData, setFormData] = useState({
+        Username: '',
+        Password: '',
+    })
+
+    const {Username, Password} = formData;
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value} = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
 
     const handleSubmit = async () => {
 
         try{
-            const response = await axios.post("/admin/login" )
+            const response = await axios.post("/admin/login", formData )
 
             setMessage("Login successful")
-        }catch(erro){
+        }catch(err){
+
 
         }
     }
     return(
-        <div></div>
+        <div>
+            <h1> LOGIN PANEL FOR ADMIN </h1>
+            <div>
+                <form onSubmit={ handleSubmit }>
+                    <div>
+                        <label> Username | Bank Email</label>
+                        <input 
+                          type = "text"
+                          name="username"
+                          value = {Username}
+                          onChange = { onChange }
+                          required
+                        />
+                    </div>
+                    <div>
+                        <label> Password </label>
+                        <input
+                          type="password"
+                          name="Password"
+                          value = { Password}
+                          onChange = { onChange}
+                          required
+                        />
+
+                    </div>
+                    <button
+                      type="submit"
+                      disabled={loading}
+                    >
+                        { loading ? 'Logging ...' : 'Login'}
+                    </button>
+                    {message && <p style={{ color: 'green' }}> {message} </p>}
+                </form>
+            </div>
+        </div>
     )
 
 }
