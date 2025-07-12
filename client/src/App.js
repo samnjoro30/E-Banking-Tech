004c2fdbc9263e2cec83835b5c5cb0506e73a7ba
Sam njoro <samnjorokibandi@gmail.com>
2025-07-12 21:21:48 +0300

    setting global responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,8 @@ import ForgotPassword from './components/ForgotPassword';
 import VerifyOTP from './components/VerifyOtp';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
 import Auth from './pages/Auth';
-import LoginAdmin from './Admin/admin_login';
-import AdminDashboard from './pages/admin';  // Import the admin dashboard
-import AuditLogs from './components/AuditLogs';  // Import audit logs component
-import Reports from './components/Reports';  // Import reports component
-import UserManagement from './components/UserManagement';  // Import user management component
-
+import './index.css'
 // Other imports
 const socket = io('https://e-banking-tech.onrender.com');
 
@@ -42,54 +36,8 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route 
-                    path="/verify-otp"
-                    element={
-                            <VerifyOTP />
-                       } 
-                />
-                <Route 
-                    path="/dashboard"
-                    element= {
-                        // <ProtectedRoute>
-                            <Dashboard />
-                        //</ProtectedRoute>
-                    } 
-                />
-            
-            <Route 
-                    path="/admin" 
-                    element={
-                        <ProtectedRoute>
-                            <AdminDashboard />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route 
-                    path="/audit-logs" 
-                    element={
-                        <ProtectedRoute>
-                            <AuditLogs />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route 
-                    path="/reports" 
-                    element={
-                        <ProtectedRoute>
-                            <Reports />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route 
-                    path="/user-management" 
-                    element={
-                        <ProtectedRoute>
-                            <UserManagement />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route path="/admin/login" element={<LoginAdmin />} />
+                <Route path="/verify-otp" element={ <VerifyOTP />} />
+                <Route path="/dashboard" element= { <Dashboard /> } />
             </Routes>    
         </Router>
     );
