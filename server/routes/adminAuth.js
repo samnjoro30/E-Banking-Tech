39b8c6b71f01cc908aaed0009e4178228b85a124
Sam njoro <samnjorokibandi@gmail.com>
2025-09-22 14:29:55 +0300

    fixed missing  modules

--- server/routes/adminAuth.js
+++ server/routes/adminAuth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.router();
+const router = express();
 const { LoginAdmin, RegisterAdmin, AdminVerifcation } = require('../controllers/adminRegister');
 
 router.post('/admin/login', LoginAdmin);
