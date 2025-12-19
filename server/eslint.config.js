0241908d34fc402330c061946156d8dea31d0cc4
Sam njoro <samnjorokibandi@gmail.com>
2025-12-19 17:26:18 +0300

    eslint config file

---
+++ server/eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        languagesOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                URL: 'readonly',
+                setTimeout: 'readonly',
+                clearTimeout: 'readonly',
+                setInterval: 'readonly',
+                clearInterval: 'readonly',
+            }
+        },
+        rules: {
+            indent: ['error', 2, { SwitchCase: 1 }],
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'no-console': 'off',
+            'prefer-const': 'error',
+            'no-var': 'error',
+            'object-shorthand': 'error',
+            'prefer-arrow-callback': 'error',
+        },
+    },
+    {
+        files: ['tests/**/*.js'],
+        languageOptions: {
+          globals: {
+            describe: 'readonly',
+            it: 'readonly',
+            expect: 'readonly',
+            beforeEach: 'readonly',
+            afterEach: 'readonly',
+            beforeAll: 'readonly',
+            afterAll: 'readonly',
+            jest: 'readonly',
+          },
+        },
+      },
+      {
+        ignores: ['node_modules/**', 'coverage/**', 'logs/**', 'drizzle/**'],
+      },
+];
