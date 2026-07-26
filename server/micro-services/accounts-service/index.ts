74ff0d623c32ab7864fbd599c88ec31c2b1e0502
Sam njoro <samnjorokibandi@gmail.com>
2026-07-26 23:50:26 +0300

    fixed format and linting

--- server/micro-services/accounts-service/index.ts
+++ server/micro-services/accounts-service/index.ts
@@ -1,20 +1,17 @@
-
-import express from "express";
-import http from "http";
-import morgan from "morgan";
-
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
 
 async function startAccountsService() {
+  const app = express();
+  const server = http.createServer(app);
 
-    const app = express();
-    const server = http.createServer(app);
-
-    server.listen(5000, () => {
-        console.log("Accounts service is running on port 5000");
-    })
+  server.listen(5000, () => {
+    console.log('Accounts service is running on port 5000');
+  });
 }
 
-startAccountsService().catch((err) => {
-    console.error("Error starting accounts service:", err);
-    process.exit(1);
+startAccountsService().catch(err => {
+  console.error('Error starting accounts service:', err);
+  process.exit(1);
 });
