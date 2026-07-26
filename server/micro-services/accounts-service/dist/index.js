1e3375051f2322c09792d0936feeb09f1c2bac36
Sam njoro <samnjorokibandi@gmail.com>
2026-07-26 23:27:33 +0300

    package installing and server accounts setup

---
+++ server/micro-services/accounts-service/dist/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import http from "http";
+import morgan from "morgan";
+async function startAccountsService() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(5000, () => {
+        console.log("Accounts service is running on port 5000");
+    });
+}
+//# sourceMappingURL=index.js.map
